4d2a34fedf5e99d58e337fa82d9be11b37133d9e
mariescimino <mariescimino@outlook.com>
2023-09-05 13:12:52 -0300

    nueva carpeta

---
+++ TomaMate_/routes/index.js
@@ -0,0 +1,43 @@
+var express = require('express');
+var router = express.Router();
+var nodemailer= require('nodemailer');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.post('/', async(req, res, next)=>{
+  
+  console.log(req.body) //estoy cpaturando los datos?
+
+  var nombre= req.body.nombre;
+  var apellido= req.body.apellido;
+  var email= req.body.email;
+  var tel= req.body.tel;
+  var mensaje= req.body.mensaje;
+
+  var obj= {
+    to:'mariescimino@gmail.com',
+    subject: 'Contacto desde la web',
+    hmtl: nombre + " " + apellido + " se contacta a traves y quiere mas info a este correo: " + email + ". <br> Ademas, hizo el siguiente mensaje: " + mensaje +". <br> Su tel es " + tel
+  }
+  
+  var transporter= nodemailer.createTransport ({
+    host: process.env.SMTP_HOST,
+    port: process.env.SMTP_PORT,
+    auth: {
+      user: process.env.SMTP_USER,
+      pass: process.env.SMTP_PASS
+    }
+ 
+  })
+  var info = await transporter.sendMail(obj);
+
+  res.render('index', {
+    message: 'Mensaje enviado correctamente'  
+  })
+
+}) // cierro peticion de post 
+
+module.exports = router;
